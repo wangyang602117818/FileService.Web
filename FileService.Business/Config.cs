b7b670bec9465fc2eba472980d17f3680f66999f
wangyang <602117818@qq.com>
2018-05-12 09:26:48 +0800

    0

--- FileService.Business/Config.cs
+++ FileService.Business/Config.cs
@@ -15,21 +15,24 @@ namespace FileService.Business
         public bool CheckFileExtension(string extension)
         {
             BsonDocument document = mongoData.FindByExtension(extension);
-            if (document == null || document["Action"].AsString.ToLower() == "block") return false;
+            if (document == null) return true;
+            if (document["Action"].AsString.ToLower() == "block") return false;
             return true;
         }
         public bool CheckFileExtensionVideo(string extension)
         {
             BsonDocument document = mongoData.FindByExtension(extension);
-            if (document == null || document["Action"].AsString.ToLower() == "block") return false;
+            if (document == null) return true;
             if (document["Type"].AsString.ToLower() != "video") return false;
+            if (document["Action"].AsString.ToLower() == "block") return false;
             return true;
         }
         public bool CheckFileExtensionImage(string extension)
         {
             BsonDocument document = mongoData.FindByExtension(extension);
-            if (document == null || document["Action"].AsString.ToLower() == "block") return false;
+            if (document == null) return true;
             if (document["Type"].AsString.ToLower() != "image") return false;
+            if (document["Action"].AsString.ToLower() == "block") return false;
             return true;
         }
         public string GetTypeByExtension(string extension)
