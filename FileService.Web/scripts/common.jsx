8a527465435d009482a71a5b771a24ed2ad1cc84
wangyang <18518317472@163.com>
2018-06-20 18:02:33 +0800

    0

--- FileService.Web/scripts/common.jsx
+++ FileService.Web/scripts/common.jsx
@@ -175,12 +175,14 @@ class DropDownList extends React.Component {
             Department: []
         }
     }
+    groupChecked(name, code) {
+
+    }
     ddlClick(e) {
-        if (e.target.nodeName == "svg") {
+        if (e.target.nodeName.toLowerCase() == "i") {
             var name = e.target.getAttribute("node-name");
             var code = e.target.getAttribute("node-code");
-            this.dataNodeIterate(this.state.Department, name, code, 1, "btn");
-            console.log(this.state.Department);
+            this.dataNodeIterate(this.state.Department, name, code, 1, "i");
             this.setState({ Department: this.state.Department });
         }
         if (e.target.nodeName.toLowerCase() == "div" && e.target.className == "node") {
@@ -194,15 +196,16 @@ class DropDownList extends React.Component {
         for (var i = 0; i < departments.length; i++) {
             if (clickLayer) {
                 if (layer > clickLayer) {
-                    departments[i].Collapse = !departments[i].Collapse;
-                    departments[i].Select = !departments[i].Select;
+                    if (type == "i") departments[i].Collapse = !departments[i].Collapse;  //用于判断是否隐藏
+                    if (type == "select") departments[i].Select = !departments[i].Select;  //用于判断是否选中
                 } else {
                     clickLayer = null;
                 }
             }
             if (departments[i].DepartmentCode == code) {
-                if (type == "btn") {
-                    //departments[i].Collapse = !departments[i].Collapse;
+                if (type == "i") {
+                    //departments[i].Collapse = !departments[i].Collapse;  //用于判断是否隐藏
+                    departments[i].virtualCollapse = !departments[i].virtualCollapse; //用于修改页面
                 }
                 if (type == "select") {
                     departments[i].Select = !departments[i].Select;
@@ -270,7 +273,8 @@ class DropDownListIterate extends React.Component {
                     if (item.Collapse) collapse = true;
                     var select = false;
                     if (item.Select) select = true;
-
+                    virtualCollapse = false;
+                    if (item.virtualCollapse) virtualCollapse = true;
                     return (
                         <React.Fragment key={i}>
                             <DropDownLine
@@ -283,6 +287,7 @@ class DropDownListIterate extends React.Component {
                                 index={i}
                                 layer={this.props.layer}
                                 collapse={collapse}
+                                virtualCollapse={virtualCollapse}
                                 select={select}
                                 layerAbsolute={this.props.layerAbsolute + "-" + i}
                             />
@@ -304,20 +309,21 @@ class DropDownLine extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         var html = "";
         var layer = this.props.layer;
         for (var i = 0; i <= layer; i++) {
+            var fonttype = "iconfont icon-delete1";
+            if (this.props.virtualCollapse) fonttype = "iconfont icon-add1";
             if (layer == i) { //最后一个
-                if (this.props.subCount > 0) {
-                    html += '<div class="node_wrap"><div class="node_main"><div class="line_wrap v_wrap"></div><div class="node" node-name=' + this.props.department.DepartmentName + ' node-code=' + this.props.department.DepartmentCode + ' > ' + this.props.department.DepartmentName + '</div > <div class="line_wrap v_wrap"><span class="v_line"></span></div></div ></div > ';
+                if (this.props.subCount > 0 && !this.props.virtualCollapse) {
+                    html += '<div class="node_wrap"><div class="node_main"><div class="line_wrap v_wrap"></div><div class="node" node-name=' + this.props.department.DepartmentName + ' node-code=' + this.props.department.DepartmentCode + ' > ' + this.props.department.DepartmentName + '</div><div class="line_wrap v_wrap"><span class="v_line"></span></div></div ></div>';
                 } else {
                     html += '<div class="node_wrap"><div class="node_main"><div class="line_wrap v_wrap"></div><div class="node" node-name=' + this.props.department.DepartmentName + ' node-code=' + this.props.department.DepartmentCode + ' >' + this.props.department.DepartmentName + '</div><div class="line_wrap v_wrap"></div></div></div>';
                 }
             } else if (layer - 1 == i) { //倒数第二个
                 if (this.props.subCount > 0) {
-                    html += '<div class="node_wrap_btn"><div class="line_wrap h_wrap_flex"></div><div class="btn_wrap"><div class="line_wrap v_wrap_flex"><span class="v_line"></span></div><div class="btn"><svg node-name=' + this.props.department.DepartmentName + ' node-code=' + this.props.department.DepartmentCode + ' layer-absolute=' + this.props.layerAbsolute + ' viewBox="0 0 1024 1024" width="16" height="16"><path d="M512 12C236.31 12 12 236.3 12 512s224.31 500 500 500 500-224.3 500-500S787.69 12 512 12z m0 944.44C266.94 956.44 67.56 757.06 67.56 512S266.94 67.56 512 67.56 956.44 266.94 956.44 512 757.06 956.44 512 956.44z" p-id="1765"></path><path d="M762 484.22H262a27.78 27.78 0 0 0 0 55.56h500a27.78 27.78 0 0 0 0-55.56z" p-id="1766"></path></svg></div><div class="line_wrap v_wrap_flex">';
+                    html += '<div class="node_wrap_btn"><div class="line_wrap h_wrap_flex"></div><div class="btn_wrap"><div class="line_wrap v_wrap_flex"><span class="v_line"></span></div><div class="btn"><i class="' + fonttype+'" node-name=' + this.props.department.DepartmentName + ' node-code=' + this.props.department.DepartmentCode + ' layer-absolute=' + this.props.layerAbsolute + '></i></div><div class="line_wrap v_wrap_flex">';
                     if (!this.props.isEnd) html += '<span class="v_line"></span>';
                     html += '</div></div><div class="line_wrap h_wrap_flex"><div class="h_line"></div></div></div>';
                 } else {
@@ -344,9 +350,9 @@ class DropDownLine extends React.Component {
             }
         }
 
-
         return (
             <div className="ddl_line"
+                style={{ display: this.props.collapse ? "none" : "block" }}
                 sub-count={this.props.subCount}
                 top-layer-count={this.props.topLayerCount}
                 layer={layer}
